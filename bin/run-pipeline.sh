cf3242fd24afbdcb9d1e376b2350e37b33e94735
ilmari-koria <ilmarikoria@posteo.net>
2025-05-31 13:52:17 +0100

    adjust CP

--- bin/run-pipeline.sh
+++ bin/run-pipeline.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-CURRENT_SCRIPT=$0
+CURRENT_SCRIPT="$0"
+WD="$(cd "$(dirname "$CURRENT_SCRIPT")" && pwd)"
 
-WD=$(dirname $CURRENT_SCRIPT)
+TMP="$WD/../tmp"
+SRC="$WD/../src"
+LIB="$WD/../lib"
 
-TMP=$WD/../tmp
-SRC=$WD/../src
-LIB=$WD/../lib/jar/*
+MORGANA="$WD/../lib/morgana"
+# TODO is $MORGANA_LIB a hard requiremenent?
+MORGANA_LIB="$WD/../lib/morgana/MorganaXProc-IIIse_lib" 
+MORGANA_CONFIG="$WD/../config/config-morgana.xml"
 
-MORGANA_CONFIG=$WD/../config/config-morgana.xml
-
+# TODO maybe rm these
 JAVA_AGENT=""
-JAVA_VER=$(java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*/\1\2/p;')
+JAVA_VER=$(java -version 2>&1 | sed -n 's/.* version "\(.*\)\.\(.*\)\..*".*/\1\2/p')
 
-if [ $JAVA_VER = "18" ]
-  then
-    JAVA_AGENT=-javaagent:$LIB/quasar-core-0.7.9.jar
-  fi
+if [ "$JAVA_VER" = "18" ]; then
+  JAVA_AGENT="-javaagent:$MORGANA_LIB/quasar-core-0.7.9.jar"
+fi
 
-CLASSPATH=$LIB/MorganaXProc-IIIse.jar:$LIB
+CLASSPATH="$MORGANA/MorganaXProc-IIIse.jar:$MORGANA_LIB/*"
 
-java $JAVA_AGENT -cp $CLASSPATH com.xml_project.morganaxproc3.XProcEngine \
-    -config=$MORGANA_CONFIG \
-    $SRC/xpl/main-pipeline.xpl
-    -input:source=$TMP/example-post/example-post.txt \
-    -output:result=$TMP/publish/example-post.html
+java $JAVA_AGENT -cp "$CLASSPATH" com.xml_project.morganaxproc3.XProcEngine \
+     -config=$MORGANA_CONFIG \
+     "$SRC/xpl/main-pipeline.xpl" \
+     -input:source="$TMP/example-post/example-post.txt" \
+     -output:result="$TMP/publish/example-post.html"
